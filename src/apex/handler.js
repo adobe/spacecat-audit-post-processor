340d0fb201f6ec58bb18a1d96d8e33410bf575d1
ekremney <ekdogan@adobe.com>
2024-01-16 16:49:23 +0100

    feat: apex audit reporting

---
+++ src/apex/handler.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { hasText } from '@adobe/spacecat-shared-utils';
+import { badRequest, internalServerError, noContent } from '@adobe/spacecat-shared-http-utils';
+import { markdown, postSlackMessage, section } from '../support/slack.js';
+
+function buildSlackMessage(url) {
+  const blocks = [];
+
+  blocks.push(section({
+    text: markdown(`*${url}* does not redirect to any subdomain (ie 'www')`),
+  }));
+
+  return blocks;
+}
+
+function isValidMessage(message) {
+  return hasText(message.url)
+    && hasText(message.auditContext?.slackContext?.channel)
+    && typeof message.auditResult?.success === 'boolean';
+}
+
+export default async function apexHandler(message, context) {
+  const { log } = context;
+  const { url, auditResult, auditContext } = message;
+  const { env: { SLACK_BOT_TOKEN: token } } = context;
+
+  if (!isValidMessage(message)) {
+    return badRequest('Required parameters missing in the message body');
+  }
+
+  if (auditResult.success) {
+    log.info(`Apex audit was successful for ${url}. Won't notify.`);
+    return noContent();
+  }
+
+  const { channel, ts } = auditContext.slackContext;
+
+  try {
+    // send alert to the slack channel - group under a thread if ts value exists
+    await postSlackMessage(token, {
+      blocks: buildSlackMessage(url),
+      channel,
+      ts,
+    });
+  } catch (e) {
+    log.error(`Failed to send Slack message for ${url}. Reason: ${e.message}`);
+    return internalServerError(`Failed to send Slack message for ${url}`);
+  }
+
+  log.info(`Slack notification sent for ${url}`);
+
+  return noContent();
+}
---
+++ test/apex/handler.test.js
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* eslint-env mocha */
+import sinon from 'sinon';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
+import nock from 'nock';
+import apex from '../../src/apex/handler.js';
+import { slackApexRequestData } from '../fixtures/slack-apex-request-data.js';
+import { getQueryParams } from '../../src/support/slack.js';
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+const sandbox = sinon.createSandbox();
+
+describe('cwv handler', () => {
+  let message;
+  let context;
+
+  beforeEach('setup', () => {
+    message = {
+      url: 'space.cat',
+      auditContext: {
+        slackContext: {
+          channel: 'channel-id',
+          ts: 'thread-id',
+        },
+      },
+      auditResult: { success: true },
+    };
+    context = {
+      log: console,
+      env: {
+        SLACK_BOT_TOKEN: 'token',
+      },
+    };
+  });
+
+  afterEach('clean', () => {
+    sandbox.restore();
+    nock.cleanAll();
+  });
+
+  it('rejects when url is missing', async () => {
+    delete message.url;
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('rejects when auditContext is missing', async () => {
+    delete message.auditContext;
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('rejects when audit result success is missing', async () => {
+    delete message.auditResult;
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('rejects when audit result success is not boolean', async () => {
+    message.auditResult.success = 'asd';
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('rejects when slack context is missing', async () => {
+    delete message.auditContext.slackContext;
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('rejects when slack channel id is missing', async () => {
+    delete message.auditContext.slackContext.channel;
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(400);
+  });
+
+  it('sends no slack message when audit was successful', async () => {
+    const logSpy = sandbox.spy(context.log, 'info');
+
+    const scope = nock('https://slack.com')
+      .get('/api/chat.postMessage')
+      .query(true)
+      .reply(200);
+
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(204);
+    expect(scope.isDone()).to.be.false; // make sure slack api is not called
+    expect(logSpy).to.have.been.calledWith('Apex audit was successful for space.cat. Won\'t notify.');
+  });
+
+  it('builds and sends the slack message when apex failed to resolve', async () => {
+    message.auditResult = { success: false };
+    const logSpy = sandbox.spy(context.log, 'info');
+    const { channel, ts } = message.auditContext.slackContext;
+
+    nock('https://slack.com', {
+      reqheaders: {
+        authorization: `Bearer ${context.env.SLACK_BOT_TOKEN}`,
+      },
+    })
+      .get('/api/chat.postMessage')
+      .query(getQueryParams(slackApexRequestData, channel, ts))
+      .reply(200, {
+        ok: 'success',
+        channel: 'ch-1',
+        ts: 'ts-1',
+      });
+
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(204);
+    expect(logSpy).to.have.been.calledWith(`Slack notification sent for ${message.url}`);
+  });
+
+  it('sending the slack message fails', async () => {
+    message.auditResult = { success: false };
+    const errorLogSpy = sandbox.spy(context.log, 'error');
+    const { channel, ts } = message.auditContext.slackContext;
+
+    nock('https://slack.com', {
+      reqheaders: {
+        authorization: `Bearer ${context.env.SLACK_BOT_TOKEN}`,
+      },
+    })
+      .get('/api/chat.postMessage')
+      .query(getQueryParams(slackApexRequestData, channel, ts))
+      .reply(500, 'invalid-');
+
+    const resp = await apex(message, context);
+    expect(resp.status).to.equal(500);
+    expect(errorLogSpy).to.have.been.calledWith(`Failed to send Slack message for ${message.url}. Reason: Failed to send slack message. Status: 500`);
+  });
+});
