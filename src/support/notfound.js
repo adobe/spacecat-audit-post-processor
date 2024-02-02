6c4b101d2c390d1f4aacf663f8770a14d3717b0f
alinarublea <alina.rublea@gmail.com>
2024-02-02 18:40:44 +0100

    feat: generify 404

--- src/support/notfound.js
+++ src/support/notfound.js
@@ -79,43 +79,27 @@ export const send404Report = async ({
   return slackClient.postMessage({ ...slackContext, blocks, unfurl_links: false });
 };
 
-/*
- * Copyright 2023 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-function isWithinLast7Days(date) {
+function isWithinDays(date, numDays) {
   const now = new Date();
-  const sevenDaysAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
+  const sevenDaysAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - numDays);
   const checkedDate = new Date(date);
   return checkedDate >= sevenDaysAgo;
 }
 
 export const processLatest404Audit = (context, site, latestAudits) => {
-  const results = [];
-  let finalUrl;
-  if (latestAudits.length > 0) {
-    finalUrl = latestAudits[0].getAuditResult().finalUrl;
-    for (const latestAudit of latestAudits) {
-      if (isWithinLast7Days(latestAudit.getAuditedAt())) {
-        const auditResult = latestAudit.getAuditResult();
-        const { result } = auditResult;
-        for (const resultItem of result) {
-          results.push(resultItem);
-        }
-      }
-    }
+  if (latestAudits.length === 0) {
+    return {};
   }
-  if (results.length > 0) {
-    return {
-      results, finalUrl, context, baseUrl: site.getBaseURL(),
-    };
+  const latestAudit = latestAudits[0];
+  const { finalUrl } = latestAudit.getAuditResult();
+  if (isWithinDays(latestAudit.getAuditedAt(), 7)) {
+    const auditResult = latestAudit.getAuditResult();
+    const { result } = auditResult;
+    if (result.length > 0) {
+      return {
+        results: result, finalUrl, context, baseUrl: site.getBaseURL(),
+      };
+    }
   }
   return {};
 };
