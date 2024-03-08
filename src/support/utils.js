ce9afac95c6c8b4f8d18a7138cffe190fd2231a3
ekremney <ekdogan@adobe.com>
2024-03-08 21:58:18 +0100

    feat: auto suggestions for 404s

--- src/support/utils.js
+++ src/support/utils.js
@@ -10,11 +10,19 @@
  * governing permissions and limitations under the License.
  */
 import { context as h2, h1 } from '@adobe/fetch';
+import { hasText } from '@adobe/spacecat-shared-utils';
+
+const LANG_CODES = ['AB', 'AA', 'AF', 'SQ', 'AM', 'AR', 'HY', 'AS', 'AY', 'AZ', 'BA', 'EU', 'BN', 'DZ', 'BH', 'BI', 'BR', 'BG', 'MY', 'BE', 'KM', 'CA', 'ZH', 'CO', 'HR', 'CS', 'DA', 'NL', 'EN', 'EO', 'ET', 'FO', 'FJ', 'FI', 'FR', 'FY', 'GD', 'GL', 'KA', 'DE', 'EL', 'KL', 'GN', 'GU', 'HA', 'IW', 'HI', 'HU', 'IS', 'IN', 'IA', 'IE', 'IK', 'GA', 'IT', 'JA', 'JW', 'KN', 'KS', 'KK', 'RW', 'KY', 'RN', 'KO', 'KU', 'LO', 'LA', 'LV', 'LN', 'LT', 'MK', 'MG', 'MS', 'ML', 'MT', 'MI', 'MR', 'MO', 'MN', 'NA', 'NE', 'NO', 'OC', 'OR', 'OM', 'PS', 'FA', 'PL', 'PT', 'PA', 'QU', 'RM', 'RO', 'RU', 'SM', 'SG', 'SA', 'SR', 'SH', 'ST', 'TN', 'SN', 'SD', 'SI', 'SS', 'SK', 'SL', 'SO', 'ES', 'SU', 'SW', 'SV', 'TL', 'TG', 'TA', 'TT', 'TE', 'TH', 'BO', 'TI', 'TO', 'TS', 'TR', 'TK', 'TW', 'UK', 'UR', 'UZ', 'VI', 'VO', 'CY', 'WO', 'XH', 'JI', 'YO', 'ZU'];
 
 /* c8 ignore next 3 */
 export const { fetch } = process.env.HELIX_FETCH_FORCE_HTTP1
   ? h1()
   : h2();
+
+export function getFilename(url, filenamePrefix, fileExtension) {
+  const urlWithDotsAndSlashesReplaced = url?.replace(/\./g, '-')?.replace(/\//g, '-');
+  return `${filenamePrefix}-${urlWithDotsAndSlashesReplaced}-${new Date().toISOString().split('T')[0]}.${fileExtension}`;
+}
 
 export function convertToCSV(array) {
   if (array.length === 0) {
@@ -36,4 +44,12 @@ export function isWithinDays(date, numDays) {
   const sevenDaysAgo = new Date(now.getFullYear(), now.getMonth(), now.getDate() - numDays);
   const checkedDate = new Date(date);
   return checkedDate >= sevenDaysAgo;
+}
+
+export function containsLangCode(token) {
+  if (!hasText(token)) {
+    return false;
+  }
+  const tokens = token.length > 2 ? token.split(/-|_/) : [token];
+  return tokens.some((t) => LANG_CODES.includes(t.toUpperCase()));
 }
