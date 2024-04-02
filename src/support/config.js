ed5166a2d9212dba89f1ffe670b3625db89dda5f
Craig Hawkins <craighawki@gmail.com>
2024-04-02 12:51:52 -0700

    update idDigestReport

--- src/support/config.js
+++ src/support/config.js
@@ -14,12 +14,28 @@ import { isArray } from '@adobe/spacecat-shared-utils';
 export const getAlertConfig = (conf, alertType) => (isArray(conf?.alerts)
   ? conf?.alerts.find((alert) => alert.type === alertType) : null);
 
-export const isDigestReport = (conf, alertType) => {
-  const alertConfig = isArray(conf?.alerts)
-    ? conf?.alerts.find((alert) => alert.type === alertType)
-    : {};
-  return alertConfig?.byOrg;
+// export const isDigestReport = (conf, alertType) => {
+//   const alertConfig = isArray(conf?.alerts)
+//     ? conf?.alerts.find((alert) => alert.type === alertType)
+//     : {};
+//   return alertConfig?.byOrg;
+// };
+
+export const isDigestReport = (orgConf, siteConf, alertType) => {
+  // Check site configuration first
+  if (getAlertConfig(siteConf, alertType) !== null) {
+    return getAlertConfig(siteConf, alertType).byOrg;
+  }
+
+  // Check organization configuration if site configuration not found
+  if (getAlertConfig(orgConf, alertType) !== null) {
+    return getAlertConfig(orgConf, alertType).byOrg;
+  }
+
+  // Return false if no matching alert config found
+  return false;
 };
+
 // TODO replace current implementation with the new one
 // export const isDigestReport = (orgConf, siteConf, alertType) => {
 // if getAlertConfig for siteConf is not null/undefined return siteAlertConfig.byOrg value
